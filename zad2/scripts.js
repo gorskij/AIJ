e41cdd6b3eccd0819cb228c7ad4d5e7cfee2f46d
Mateusz <midec.p@gmail.com>
2023-10-06 14:09:28 +0200

    Add data presentation in table (5)

--- zad2/scripts.js
+++ zad2/scripts.js
@@ -27,36 +27,65 @@ let deleteTodo = function(index) {
     updateJSONbin();
 }
 
+function generateTableHead(table, data) {
+    if(data !== undefined) {
+        let thead = table.createTHead();
+        let row = thead.insertRow();
+        for (let key of data) {
+            let th = document.createElement("th");
+            let text = document.createTextNode(key);
+            th.appendChild(text);
+            row.appendChild(th);
+        }
+
+        let th = document.createElement("th");
+        let text = document.createTextNode("Remove");
+        th.appendChild(text);
+        row.appendChild(th);
+    }
+}
+
+function generateTable(table, data, filter) {
+    if(data !== undefined) {
+        for (let element of data) {
+            let row = table.insertRow();
+
+            if(filter.value === "" || element.title.includes(filter.value)
+                || element.description.includes(filter.value)) {
+                for (let key in element) {
+                    let cell = row.insertCell();
+                    let text = document.createTextNode(element[key]);
+                    cell.appendChild(text);
+                }
+
+                let cell = row.insertCell();
+
+                let newDeleteButton = document.createElement("input");
+                newDeleteButton.type = "button";
+                newDeleteButton.value = "x";
+                newDeleteButton.addEventListener("click",
+                    function () {
+                        deleteTodo(element);
+                    });
+
+                cell.appendChild(newDeleteButton);
+            }
+        }
+    }
+}
 let updateTodoList = function() {
     let todoListDiv =
         document.getElementById("todoListView");
 
-    //remove all elements
     while (todoListDiv.firstChild) {
         todoListDiv.removeChild(todoListDiv.firstChild);
     }
 
+    let keys = todoList !== undefined ? Object.keys(todoList[0]) : null;
     let filterInput = document.getElementById("inputSearch");
 
-    //add all elements
-    for (let todo in todoList) {
-        if(filterInput.value === "" || todoList[todo].title.includes(filterInput.value)
-        || todoList[todo].description.includes(filterInput.value)) {
-            let newElement = document.createElement("div");
-            let newContent = document.createTextNode(
-                todoList[todo].title + " " + todoList[todo].description);
-            let newDeleteButton = document.createElement("input");
-            newDeleteButton.type = "button";
-            newDeleteButton.value = "x";
-            newDeleteButton.addEventListener("click",
-                function () {
-                    deleteTodo(todo);
-                });
-            newElement.appendChild(newContent);
-            newElement.appendChild(newDeleteButton);
-            todoListDiv.appendChild(newElement);
-        }
-    }
+    generateTableHead(todoListDiv, keys);
+    generateTable(todoListDiv, todoList, filterInput);
 }
 
 setInterval(updateTodoList, 1000);
