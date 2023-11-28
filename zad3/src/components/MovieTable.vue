b7dcd69b4d7c956096d6bc857a18d6ba090813a2
Mateusz <midec.p@gmail.com>
2023-11-28 11:53:31 +0100

    Add "Load more" functionality to MovieTable

--- zad3/src/components/MovieTable.vue
+++ zad3/src/components/MovieTable.vue
@@ -2,11 +2,33 @@
 export default {
   name: "MovieTable",
   props: ['data'],
+  data() {
+    return {
+      itemsToShow: 10,
+      visibleData: [],
+    }
+  },
+  watch: {
+    data: function() {
+      this.initData();
+    }
+  },
+  methods: {
+    initData() {
+      this.itemsToShow = 10;
+      this.visibleData = this.data.slice(0, this.itemsToShow);
+    },
+    handleShowMore() {
+      const endIndex = this.visibleData.length + this.itemsToShow;
+      this.visibleData = this.data.slice(0, endIndex);
+    }
+  }
 }
 
 </script>
 
 <template>
+  <div>
     <table>
       <thead>
       <tr>
@@ -17,7 +39,7 @@ export default {
       </tr>
       </thead>
       <tbody>
-      <tr v-for="(movie, index) in data" :key="index">
+      <tr v-for="(movie, index) in visibleData" :key="index">
         <td>{{ movie.title }}</td>
         <td>{{ movie.year }}</td>
         <td>{{ movie.cast }}</td>
@@ -25,6 +47,8 @@ export default {
       </tr>
       </tbody>
     </table>
+  <button @click="handleShowMore" v-if="visibleData.length < data.length">Pokaż więcej</button>
+  </div>
 </template>
 
 <style scoped>
