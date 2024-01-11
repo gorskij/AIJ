9bae350e407084e771cf72cbf2e9e75c88357e35
Mateusz <midec.p@gmail.com>
2024-01-11 19:42:24 +0100

    Add cart icon with product count

--- zad4/web/src/components/cart/Cart.tsx
+++ zad4/web/src/components/cart/Cart.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import {CartProps} from "./CartType";
+import CartIcon from "./shopping-cart-2.svg";
+import styles from "./Cart.module.css";
 
 function Cart({products}: CartProps) {
     return (
-        <div>
-            Product amount: {products.length}
+        <div className={styles.cart}>
+            <img className={styles.icon} src={CartIcon} alt="Koszyk"/>
+            <div className={styles.amount}>{products.length}</div>
         </div>
     );
 }
