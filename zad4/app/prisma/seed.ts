744dbdfd123995ad3ebb4e0975c905b8c5984213
Jakub Górski <242396@edu.p.lodz.pl>
2024-01-17 11:15:33 +0100

    feat: products seeder

--- zad4/app/prisma/seed.ts
+++ zad4/app/prisma/seed.ts
@@ -23,9 +23,55 @@ async function main() {
         name: 'RTV',
       },
     });
+
+    const products = await prisma.product.createMany({
+      data: [
+        {
+          name: "TV",
+          description: "Telewizor",
+          price: 600,
+          weight: 10,
+          categoryId:2,
+        },
+        {
+          name: "Monitor",
+          description: "Mały telewizor",
+          price: 400,
+          weight: 10,
+          categoryId:2,
+        },
+        {
+          name: "Mikrofala",
+          description: "MMMMM",
+          price: 300,
+          weight: 100,
+          categoryId:1,
+        },
+        {
+          name: "PS6",
+          description: "Konsoleta",
+          price: 30000,
+          weight: 1,
+          categoryId:2,
+        },
+        {
+          name: "Lodówka",
+          description: "Zimno",
+          price: 200,
+          weight: 1000,
+          categoryId:1,
+        },
+        {
+          name: "Drukarka",
+          description: "print print",
+          price: 50,
+          weight: 10,
+          categoryId:2,
+        },
+        
+      ],
+    });
     
-    console.log('Inserted order statuses:', orderStatuses);
-    console.log('Inserted categories:', category1, category2);
   } catch (error) {
     console.error('Error inserting categories:', error);
   } finally {
